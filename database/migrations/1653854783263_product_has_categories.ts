b74ef201a9516674d9d0a5716c28b30672244e15
Guilhermeslv <guilhermeslv78@gmail.com>
2022-05-29 22:48:14 -0300

    "Migrations de relações N para N"

---
+++ database/migrations/1653854783263_product_has_categories.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'product_has_categories'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.integer('product_id').unsigned().references('id').inTable('products').onDelete('CASCADE')
+      table.integer('category_id').unsigned().references('id').inTable('categories').onDelete('CASCADE')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
